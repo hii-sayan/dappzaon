de176dfcb184a3c17dce35f895dcdc7b1fc480bc
Sayantan <sonusayantan25@gmail.com>
2024-02-27 03:29:04 +0530

    first

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappcord {}
+contract Dappazon {
+  address public owner;
+
+  struct Item {
+    uint id;
+    string name;
+    string category;
+    string image;
+    uint256 cost;
+    uint256 rating;
+    uint256 stock;
+  }
+
+  mapping(uint256 => Item) public items;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  //List products
+  function list (
+   uint256 _id,
+   string memory _name,
+   string memory _category,
+   string memory _image,
+   uint256 _cost,
+   uint256 _rating,
+   uint256 _stock
+   )public {
+    //Create Item struct
+    Item memory item = Item(
+      _id,
+      _name,
+      _category,
+      _image,
+      _cost,
+      _rating,
+      _stock
+      );
+
+    //Save Item Struct to blockchain
+    items[_id] = item;
+
+
+  }
+
+  //Buy products
+
+  //Withdraw products
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,43 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let dappazon
+  let deployer, buyer
+
+  beforeEach(async () =>{
+    //Setuo accounts
+    [deployer, buyer] = await ethers.getSigners()
+
+    //Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+
+  })
+
+  describe("Deloyment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+  })
+  describe("Listing", () => {
+    let transaction
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(
+        1,
+        "Shoes",
+        "Clothing",
+        "IMAGE",
+        1,
+        4,
+        5
+      )
+      await transaction.wait()
+    })
+    it("Returns item attribute", async () => {
+      const item = await dappazon.items(1)
+      expect(item.id).to.equal(1)
+    })
+  })
 
 })
